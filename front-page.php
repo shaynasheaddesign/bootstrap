72f5168591f8cef165999825e350af09b7e926a8
shaynasheaddesign <31190275+shaynasheaddesign@users.noreply.github.com>
2017-08-20 20:32:12 -0600

    front-page

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Front page template
+ *
+ * @package      Bootstrap
+ * @author       shaynashead
+ * @link         http://www.shaynashead.com/
+ * @copyright    Copyright (c) 2017, Shayna SHead
+ * @license      GPL-3.0+
+ */
+
+genesis();
