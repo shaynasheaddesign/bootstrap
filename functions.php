ef75d35d4f8258a980ee03bb66d5478072b250e2
shaynasheaddesign <31190275+shaynasheaddesign@users.noreply.github.com>
2017-08-20 20:16:29 -0600

    added files

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Theme customizations
+ *
+ * @package      Bootstrap
+ * @author       shaynashead
+ * @link         http://www.shaynashead.com/
+ * @copyright    Copyright (c) 2017, Shayna SHead
+ * @license      GPL-3.0+
+ */
+ // Load child theme textdomain.
+ load_child_theme_textdomain( 'bootstrap' );
+
+ add_action( 'genesis_setup', 'bootstrap_setup' );
+ /**
+  * Theme setup.
+  *
+  * Attach all of the site-wide functions to the correct hooks and filters. All
+  * the functions themselves are defined below this setup function.
+  *
+  * @since 1.0.0
+  */
+ function bootstrap_setup() {
+
+ }
